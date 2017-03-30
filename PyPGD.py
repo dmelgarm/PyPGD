470b25adb599db6f1d9664c7d722c3d2705a136a
Diego Melgar <dmelgar@berkeley.edu>
2017-03-30 08:16:54 -0700

    Added SNR to event class

--- PyPGD.py
+++ PyPGD.py
@@ -4,7 +4,7 @@ class Event(object):
     Things related to a particular earthquakes
     '''
 
-    def __init__(self,path_to_data,path_to_station_file,hypocenter,hypo_time):
+    def __init__(self,path_to_data,path_to_station_file,hypocenter,hypo_time,duration):
         
         '''
         Initalize the class
@@ -14,11 +14,14 @@ class Event(object):
         
         self.hypocenter=hypocenter
         self.hypo_time=hypo_time
-        self.path_to_data=path_to_data
+        self.path_to_data=path_to_data+'/proc/'
         self.path_to_station_file=path_to_station_file
+        self.duration=duration
         self.station_locations=genfromtxt(self.path_to_station_file,usecols=[1,2])
         self.station_names=genfromtxt(self.path_to_station_file,usecols=0,dtype='S')
+        self.Nsta=len(self.station_names)
         
+    
     def get_travel_times(self):
         '''
         Get direct P and S to each station
@@ -29,12 +32,11 @@ class Event(object):
         from obspy.geodetics import locations2degrees
         from datetime import timedelta
         
-        Nsta=len(self.station_names)
 
-        self.ptime=1e6*ones(Nsta)
-        self.stime=1e6*ones(Nsta)        
+        self.ptime=1e6*ones(self.Nsta)
+        self.stime=1e6*ones(self.Nsta)        
                         
-        for k in range(Nsta):
+        for k in range(self.Nsta):
         
             #tauP class
             velmod = TauPyModel()
@@ -56,8 +58,82 @@ class Event(object):
                 if 'S' == arrivals[kphase].name or 's' == arrivals[kphase].name or 'Sn' == arrivals[kphase].name:
                     if arrivals[kphase].time<self.stime[k]:
                         self.stime[k]=arrivals[kphase].time
+                        
+    
+    def get_SNR(self,t_prior=60,source_durations=1.5):
+        '''
+        Get SNR considering t_prior seconds before p-wave and a
+        total waveofrm of source_durations length
         
+        '''
         
+        from numpy import zeros,log10
+        from obspy import read
+        from datetime import timedelta
+        from obspy import UTCDateTime
+        
+        self.SNR_north=zeros(self.Nsta)  
+        self.SNR_east=zeros(self.Nsta)   
+        self.SNR_up=zeros(self.Nsta) 
+        
+        hypo_time=UTCDateTime(self.hypo_time)
+        
+        for k in range(self.Nsta):
+            path=self.path_to_data
+            
+            ptime=self.ptime[k]
+
+            tp=hypo_time+timedelta(seconds=ptime)  
+            t0=tp-timedelta(seconds=t_prior)
+            tfinal=tp+timedelta(seconds=source_durations*self.duration)          
+                                    
+            #read waveforms
+            n=read(path+self.station_names[k]+'.LXN.sac')
+            e=read(path+self.station_names[k]+'.LXE.sac')
+            z=read(path+self.station_names[k]+'.LXZ.sac')
+            
+            #Get pre-event portions (from t0 to tp)
+            n_pre=n.copy()
+            e_pre=e.copy()
+            z_pre=z.copy()
+            
+            n_pre[0].trim(starttime=t0,endtime=tp)
+            e_pre[0].trim(starttime=t0,endtime=tp)
+            z_pre[0].trim(starttime=t0,endtime=tp)
+            
+            mean_n=n_pre[0].data.mean()
+            mean_e=e_pre[0].data.mean()
+            mean_z=z_pre[0].data.mean()
+            
+            n_pre[0].data=n_pre[0].data-mean_n
+            e_pre[0].data=e_pre[0].data-mean_e
+            z_pre[0].data=z_pre[0].data-mean_z
+            
+            #get power
+            Pn_pre=sum(n_pre[0].data**2)/n_pre[0].stats.npts
+            Pe_pre=sum(e_pre[0].data**2)/e_pre[0].stats.npts
+            Pz_pre=sum(z_pre[0].data**2)/z_pre[0].stats.npts
+            
+            #Trim post P-arrival signal
+            n[0].trim(starttime=tp,endtime=tfinal)
+            e[0].trim(starttime=tp,endtime=tfinal)
+            z[0].trim(starttime=tp,endtime=tfinal)
+            
+            n[0].data=n[0].data-mean_n
+            e[0].data=e[0].data-mean_e
+            z[0].data=z[0].data-mean_z
+            
+            #get power
+            Pn=sum(n[0].data**2)/n[0].stats.npts
+            Pe=sum(e[0].data**2)/e[0].stats.npts
+            Pz=sum(z[0].data**2)/z[0].stats.npts
+            
+            #And finaly SNR
+            self.SNR_north[k]=Pn/Pn_pre
+            self.SNR_east[k]=Pe/Pe_pre
+            self.SNR_up[k]=Pz/Pz_pre
+               
+
 
 class pgd(object):
     '''
@@ -80,15 +156,39 @@ class pgd(object):
         self.event_hypocenters=genfromtxt(self.catalog_file,usecols=[2,3,4])
         self.magnitudes=genfromtxt(self.catalog_file,usecols=5)
         self.Nevents=len(self.event_names)
+        self.durations=genfromtxt(self.catalog_file,usecols=6)
         
         #Create event objects
         self.event=[]
         for k in range(self.Nevents):
             hypo=self.event_hypocenters[k]
-            hypo_time=self.event_epicentral_times
+            hypo_time=self.event_epicentral_times[k]
+            duration=self.durations[k]
             path_to_data=self.path_to_files+self.event_names[k]
             path_to_station_file=self.path_to_files+'_station_info/'+self.event_names[k]+'.sta'
-            self.event.append(Event(path_to_data,path_to_station_file,hypo,hypo_time))
+            self.event.append(Event(path_to_data,path_to_station_file,hypo,hypo_time,duration))
+            
+    
+    
+    def get_travel_times(self):
+        '''
+        Get travel times for all events
+        '''
+        
+        for k in range(self.Nevents):
+            print '... calculating phases for event '+self.event_names[k]
+            self.event[k].get_travel_times()
+            
+            
+            
+    def get_SNR(self):
+        '''
+        Get travel times for all events
+        '''
+        
+        for k in range(self.Nevents):
+            print '... calculating SNR for all stations for event '+self.event_names[k]
+            self.event[k].get_SNR()
 
         
 
--- scripts/run_stuff.py
+++ scripts/run_stuff.py
@@ -2,3 +2,13 @@ from PyPGD import pgd
 
 path_to_files=u'/Users/dmelgar/PGD_gmpe/'
 p=pgd(path_to_files)
+
+
+# Get hypocentral distance too DOESN'T EXIST YET
+p.get_hypocentral_distance()
+
+#get travel times THIS IS NOT WORKING FOR ALL EVENTS tohoku lon lat columns are transposed
+p.get_travel_times()
+
+#get SNR
+p.get_SNR()
